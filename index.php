7baf2d315770da996e8676d41bf48996814ffcac
Ronnel <logdatronnell@gmail.com>
2026-02-19 21:46:05 +0800

    added a build script for package.json

--- index.php
+++ index.php
@@ -3,13 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="src/input.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="./dist/output.css">
+
+    <title>TEST</title>
 </head>
 <body>
-    <h1>Welcome to the CRUD Application</h1>
-    <?php 
-
-    ?>
+    <h1 class = "text-xl font-bold text-amber-300">Insert New Record</h1>
+    <button class="bg-blue-500 text-black px-4 rounded-lg p-2">Click Me</button>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   content: [
     "./**/*.php",
+        "./*.php",
+    "./*.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
